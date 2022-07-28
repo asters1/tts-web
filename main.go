f7b83a378eb99bdff08fa92e83707e06b57fe59a
asters1 <gj744559140@gmail.com>
2022-07-28 19:36:06 +0800

    修改细节

--- main.go
+++ main.go
@@ -23,33 +23,51 @@ func GetISOTime() string {
 	return T[:23][:10] + "T" + T[:23][11:] + "Z"
 
 }
+func NewConn() *websocket.Conn {
+
+	fmt.Println("获取token...")
+	token := GetToken()
+	uuid := tools.GetUUID()
+	WssUrl := `wss://all.wisteria.cf/eastus.tts.speech.microsoft.com/cognitiveservices/websocket/v1?Authorization=` + token + `&X-ConnectionId=` + uuid
+	dl := websocket.Dialer{
+		EnableCompression: true,
+	}
+
+	fmt.Println("创建websocket连接...")
+	conn, _, err := dl.Dial(WssUrl, tools.GetHeader(
+		`Accept-Encoding:gzip
+		User-Agent:Mozilla/5.0 (Linux; Android 7.1.2; M2012K11AC Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.117 Mobile Safari/537.36
+		Origin:https://azure.microsoft.com`,
+	))
+	if err != nil {
+		fmt.Println("websocket连接失败")
+	}
+
+	m1 := "Path: speech.config\r\nX-RequestId: " + uuid + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/json\r\n\r\n{\"context\":{\"system\":{\"name\":\"SpeechSDK\",\"version\":\"1.19.0\",\"build\":\"JavaScript\",\"lang\":\"JavaScript\",\"os\":{\"platform\":\"Browser/Linux x86_64\",\"name\":\"Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0\",\"version\":\"5.0 (X11)\"}}}}"
+	m2 := "Path: synthesis.context\r\nX-RequestId: " + uuid + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/json\r\n\r\n{\"synthesis\":{\"audio\":{\"metadataOptions\":{\"sentenceBoundaryEnabled\":false,\"wordBoundaryEnabled\":false},\"outputFormat\":\"audio-24khz-160kbitrate-mono-mp3\"}}}"
+	conn.WriteMessage(websocket.TextMessage, []byte(m1))
+	conn.WriteMessage(websocket.TextMessage, []byte(m2))
+	return conn
+}
 func main() {
 	ch := make(chan string)
 	check := make(chan int)
 	go func() {
-		fmt.Println("获取token...")
-		token := GetToken()
-		uuid := tools.GetUUID()
-		WssUrl := `wss://eastus.tts.speech.microsoft.com/cognitiveservices/websocket/v1?Authorization=` + token + `&X-ConnectionId=` + uuid
-		dl := websocket.Dialer{
-			EnableCompression: true,
-		}
-
-		fmt.Println("创建websocket连接...")
-		conn, _, err := dl.Dial(WssUrl, tools.GetHeader(
-			`Accept-Encoding:gzip
-		User-Agent:Mozilla/5.0 (Linux; Android 7.1.2; M2012K11AC Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.117 Mobile Safari/537.36
-		Origin:https://azure.microsoft.com`,
-		))
-		if err != nil {
-			fmt.Println("websocket连接失败")
-		}
+		conn := NewConn()
 		defer conn.Close()
-
-		m1 := "Path: speech.config\r\nX-RequestId: " + uuid + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/json\r\n\r\n{\"context\":{\"system\":{\"name\":\"SpeechSDK\",\"version\":\"1.19.0\",\"build\":\"JavaScript\",\"lang\":\"JavaScript\",\"os\":{\"platform\":\"Browser/Linux x86_64\",\"name\":\"Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0\",\"version\":\"5.0 (X11)\"}}}}"
-		m2 := "Path: synthesis.context\r\nX-RequestId: " + uuid + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/json\r\n\r\n{\"synthesis\":{\"audio\":{\"metadataOptions\":{\"sentenceBoundaryEnabled\":false,\"wordBoundaryEnabled\":false},\"outputFormat\":\"audio-24khz-160kbitrate-mono-mp3\"}}}"
-		conn.WriteMessage(websocket.TextMessage, []byte(m1))
-		conn.WriteMessage(websocket.TextMessage, []byte(m2))
+		go func() {
+			for {
+				err := conn.WriteMessage(websocket.PingMessage, []byte(""))
+				if err != nil {
+					fmt.Println("连接断开正在重连")
+					conn1 := NewConn()
+					*conn = *conn1
+				} else {
+					fmt.Println("连接正常")
+				}
+				time.Sleep(time.Second * 20)
+			}
+		}()
 		for {
 			language := <-ch
 			name := <-ch
