1e703850a3dcc92b992d3bb262d593dd4b66e090
asters1 <gj744559140@gmail.com>
2022-07-28 20:51:51 +0800

    基本框架构建完成

--- main.go
+++ main.go
@@ -5,11 +5,24 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/asters1/tools"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	uuid string
+	conn *websocket.Conn
+}
+
+var (
+	lock  sync.Mutex
+	wg    sync.WaitGroup
+	ch    chan string
+	check chan int
 )
 
 func GetToken() string {
@@ -23,96 +36,127 @@ func GetISOTime() string {
 	return T[:23][:10] + "T" + T[:23][11:] + "Z"
 
 }
-func NewConn() *websocket.Conn {
-
-	fmt.Println("获取token...")
+func GetLogTime() string {
+	BJ, _ := time.LoadLocation("Asia/Shanghai")
+	return "[" + time.Now().In(BJ).String()[:19] + "]"
+}
+func NewClient() (*Client, error) {
 	token := GetToken()
 	uuid := tools.GetUUID()
 	WssUrl := `wss://all.wisteria.cf/eastus.tts.speech.microsoft.com/cognitiveservices/websocket/v1?Authorization=` + token + `&X-ConnectionId=` + uuid
+
 	dl := websocket.Dialer{
 		EnableCompression: true,
 	}
-
-	fmt.Println("创建websocket连接...")
 	conn, _, err := dl.Dial(WssUrl, tools.GetHeader(
 		`Accept-Encoding:gzip
 		User-Agent:Mozilla/5.0 (Linux; Android 7.1.2; M2012K11AC Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.117 Mobile Safari/537.36
 		Origin:https://azure.microsoft.com`,
 	))
 	if err != nil {
-		fmt.Println("websocket连接失败")
+		return nil, err
 	}
-
 	m1 := "Path: speech.config\r\nX-RequestId: " + uuid + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/json\r\n\r\n{\"context\":{\"system\":{\"name\":\"SpeechSDK\",\"version\":\"1.19.0\",\"build\":\"JavaScript\",\"lang\":\"JavaScript\",\"os\":{\"platform\":\"Browser/Linux x86_64\",\"name\":\"Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0\",\"version\":\"5.0 (X11)\"}}}}"
 	m2 := "Path: synthesis.context\r\nX-RequestId: " + uuid + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/json\r\n\r\n{\"synthesis\":{\"audio\":{\"metadataOptions\":{\"sentenceBoundaryEnabled\":false,\"wordBoundaryEnabled\":false},\"outputFormat\":\"audio-24khz-160kbitrate-mono-mp3\"}}}"
 	conn.WriteMessage(websocket.TextMessage, []byte(m1))
 	conn.WriteMessage(websocket.TextMessage, []byte(m2))
-	return conn
+
+	return &Client{
+		uuid: uuid,
+		conn: conn,
+	}, err
 }
-func main() {
-	ch := make(chan string)
-	check := make(chan int)
-	go func() {
-		conn := NewConn()
-		defer conn.Close()
-		go func() {
+func (c Client) Close() {
+	c.conn.Close()
+}
+func CheckConn(c *Client) {
+	for {
+		err := c.conn.WriteMessage(websocket.PingMessage, []byte(""))
+		if err != nil {
+			fmt.Println(GetLogTime() + " -> 连接断开...")
 			for {
-				err := conn.WriteMessage(websocket.PingMessage, []byte(""))
-				if err != nil {
-					fmt.Println("连接断开正在重连")
-					conn1 := NewConn()
-					*conn = *conn1
-				} else {
-					fmt.Println("连接正常")
+				time.Sleep(time.Second * 5)
+				c1, err1 := NewClient()
+				if err1 == nil {
+
+					*c = *c1
+					defer c1.conn.Close()
+					fmt.Println(GetLogTime() + " -> 连接已重置...")
+					break
+
 				}
-				time.Sleep(time.Second * 20)
 			}
-		}()
-		for {
-			language := <-ch
-			name := <-ch
-			volume := <-ch
-			rate := <-ch
 
-			pitch := <-ch
-			text := <-ch
-			sjc := <-ch
+		} else {
+			fmt.Println(GetLogTime() + " -> 连接正常...")
+		}
+		time.Sleep(time.Second * 15)
+	}
+}
+func RunWebSocket() {
 
-			SSML := `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
+	fmt.Println("创建websocket连接...")
+	Client, err := NewClient()
+	if err != nil {
+
+		fmt.Println("创建websocket连接失败...")
+		return
+	} else {
+		fmt.Println("创建websocket连接成功!")
+	}
+	defer Client.Close()
+	go CheckConn(Client)
+
+	for {
+		language := <-ch
+		name := <-ch
+		volume := <-ch
+		rate := <-ch
+
+		pitch := <-ch
+		text := <-ch
+		sjc := <-ch
+
+		SSML := `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
         <voice name="` + language + `-` + name + `">
             <mstts:express-as style="general" >
                 <prosody rate="` + rate + `%" volume="` + volume + `" pitch="` + pitch + `%">` + string(text) + `</prosody>
             </mstts:express-as>
         </voice>
     </speak>`
-			m3 := "Path: ssml\r\nX-RequestId: " + tools.GetUUID() + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/ssml+xml\r\n\r\n" + SSML
-			conn.WriteMessage(websocket.TextMessage, []byte(m3))
+		m3 := "Path: ssml\r\nX-RequestId: " + tools.GetUUID() + "\r\nX-Timestamp: " + GetISOTime() + "\r\nContent-Type: application/ssml+xml\r\n\r\n" + SSML
+		Client.conn.WriteMessage(websocket.TextMessage, []byte(m3))
 
-			var Adata []byte
-			fmt.Println("正在下载文件...")
-			for {
-				Num, message, err := conn.ReadMessage()
-				if err != nil {
-					fmt.Println(err)
-					break
-				}
-				if Num == 2 {
-					index := strings.Index(string(message), "Path:audio")
-
-					data := []byte(string(message)[index+12:])
-					Adata = append(Adata, data...)
-				} else if Num == 1 && string(message)[len(string(message))-14:len(string(message))-6] == "turn.end" {
-					fmt.Println("已完成")
-					break
-				}
-
+		var Adata []byte
+		fmt.Println("正在下载文件...")
+		for {
+			Num, message, err := Client.conn.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				break
 			}
-			Adata = Adata[:len(Adata)-2400]
-			ioutil.WriteFile("./mp3/"+sjc+".mp3", Adata, 0666)
-			check <- 0
+			if Num == 2 {
+				index := strings.Index(string(message), "Path:audio")
+
+				data := []byte(string(message)[index+12:])
+				Adata = append(Adata, data...)
+			} else if Num == 1 && string(message)[len(string(message))-14:len(string(message))-6] == "turn.end" {
+				fmt.Println("已完成")
+				break
+			}
 
 		}
-	}()
+		Adata = Adata[:len(Adata)-2400]
+		ioutil.WriteFile("./mp3/"+sjc+".mp3", Adata, 0666)
+		check <- 0
+
+	}
+}
+
+func main() {
+	ch = make(chan string)
+	check = make(chan int)
+	go RunWebSocket()
 	r := gin.Default()
 	r.POST("/tts", func(c *gin.Context) {
 		ch <- c.PostForm("language")
@@ -132,11 +176,12 @@ func main() {
 		c.Redirect(http.StatusMovedPermanently, "http://localhost:8080/mp3?sjc="+sjc)
 
 	})
-	r.GET("mp3", func(ctx *gin.Context) {
+	r.GET("/mp3", func(ctx *gin.Context) {
 		sjc := ctx.Query("sjc")
 		ctx.Header("Content-Type", "audio/mpeg")
 		ctx.File("./mp3/" + sjc + ".mp3")
 
 	})
 	r.Run()
+
 }
